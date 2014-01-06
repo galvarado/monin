7ad95c57aa309ddbba6fd537b8024aa2ca22e1f7
Guillermo Alvarado <galvarado@kionetworks.com>
2014-01-06 13:31:11 -0600

    deploying to heroku

--- monin/wsgi.py
+++ monin/wsgi.py
@@ -11,6 +11,4 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monin.settings")
 application = Cling(get_wsgi_application())
