73921e1cbade90e98976d3474a85f53d29579434
Guillermo Alvarado <galvarado@kionetworks.com>
2014-04-15 16:36:30 -0500

    New pages translated

--- main/forms.py
+++ main/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from main.models import Order, ImageSlider, CategorySample, Category, Product, ProductSample, SiteConfiguration
 
