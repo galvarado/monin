42999b709d01b6c53a1d10f3391369ff3f3224d2
Guillermo Alvarado <galvarado@kionetworks.com>
2014-08-11 23:31:53 -0500

    S3ush

--- monin/settings.py
+++ monin/settings.py
@@ -154,9 +154,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 FROM = 'info@monin.com.mx'
 TO = 'omr_123@hotmail.com' #'guillermoalvarado89@gmail.com'
